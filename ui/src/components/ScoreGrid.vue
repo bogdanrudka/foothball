16a182bcb86e2fdf9edcd4085110a7234fd34d2a
bogdan.rudka <rudka.bogdan@gmail.com>
2020-04-01 22:54:19 -0500

    Adding progress monitoring

--- ui/src/components/ScoreGrid.vue
+++ ui/src/components/ScoreGrid.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
-        <b-table striped hover :fields="fields" :items="scores"/>
+        <b-table striped hover :fields="fields" :items="scores">
+            <!-- A custom formatted column -->
+            <template v-slot:cell(progress)="data">
+                <b-progress height="2em" class="mt-0" :max="maxGames" show-value>
+                    <b-progress-bar :value="data.item.wins" variant="success"></b-progress-bar>
+                    <b-progress-bar :value="data.item.totalGames - data.item.wins" variant="danger"></b-progress-bar>
+                    <b-progress-bar :value="104 - data.item.totalGames" variant="info" :striped="true"></b-progress-bar>
+                </b-progress>
+            </template>
+
+        </b-table>
     </div>
 </template>
 
@@ -16,12 +26,14 @@
                     firstName: '',
                     lastName: ''
                 },
+                maxGames: 104,
                 players: [],
                 value: [],
                 scores: [],
                 show: true,
                 fields: [
                     {key: 'name', sortable: true},
+                    {key: 'progress', sortable: false, tdClass: 'progressCol'},
                     {key: 'score', sortable: true},
                     {key: 'wins', sortable: true},
                     {key: 'pivot', sortable: true},
@@ -52,4 +64,7 @@
 </script>
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 <style>
+    .progressCol {
+        width: 40rem;
+    }
 </style>
